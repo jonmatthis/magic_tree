044344d5d01de04e58df15cd09f10d3808211648
Jon Matthis <jonmatthis@gmail.com>
2023-10-11 17:11:26 -0400

    add `MagicTree` and `demo` to __init__ w/ full-name aliases

--- magic_tree/__init__.py
+++ magic_tree/__init__.py
@@ -1,3 +1,8 @@
+from magic_tree.demo import magic_tree_demo
+from magic_tree.magic_tree_dict import MagicTreeDict
+
+MagicTree = MagicTreeDict
+demo = magic_tree_demo
 
 # set up logging
 from magic_tree.system.configure_logging import configure_logging
